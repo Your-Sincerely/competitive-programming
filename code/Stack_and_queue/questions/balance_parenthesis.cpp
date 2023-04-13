92e81bb87ddeed57488dae28de497a3bcfbcc650
Your-Sincerely <101339053+Your-Sincerely@users.noreply.github.com>
2023-04-13 19:07:43 +0530

    Add files via upload
    basic

---
+++ code/Stack_and_queue/questions/balance_parenthesis.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "C:\Users\LENOVO\Documents\DSA Coding Ninjas\code\Stack_and_queue\lectures\StackUsingArray.h"
+#include <stack>
+using namespace std;
+
+int main(){
+
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+
+    cout<<s.top()<<endl;
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+    cout<<s.size()<<endl;
+    cout<<s.empty()<<endl;
+
+}
+ 
